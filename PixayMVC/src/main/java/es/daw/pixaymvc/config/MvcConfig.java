17a655e6c7ee55868b929212fb2fc25a1a433af7
Freyja96 <cristina.oue@gmail.com>
2026-03-04 19:36:10 +0100

    esqueleto de html, MVCConfig

--- PixayMVC/src/main/java/es/daw/pixaymvc/config/MvcConfig.java
+++ PixayMVC/src/main/java/es/daw/pixaymvc/config/MvcConfig.java
@@ -8,12 +8,23 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry){
-        registry.addViewController("/").setViewName("home");
-        registry.addViewController("/home").setViewName("home");
-        //registry.addViewController("/").setViewName("login");
+        registry.addViewController("/").setViewName("inicio");
+        registry.addViewController("/inicio").setViewName("inicio");
         registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/registro").setViewName("registro");
         registry.addViewController("/error").setViewName("error");
-        //registry.addViewController("/dashboard").setViewName("dashboard")
+        registry.addViewController("{id}").setViewName("{id}");
+        //TODO no sé si sería así para mirar el perfil de otros usuarios y si no podríamos aprovechar así para el propio
+        // usuario también
+        registry.addViewController("{id}/imagen/{id-imagen}").setViewName("imagen/{id-imagen}");
+        registry.addViewController("{id}/mis-imagenes").setViewName("mi-perfil/mis-imagenes");
+        registry.addViewController("{id}/mis-imagenes/nueva-imagen").setViewName("mi-perfil/mis-imagenes/nueva-imagen");
+        registry.addViewController("{id}/editar-perfil").setViewName("mi-perfil/editar-perfil");
+        registry.addViewController("{id}/imagenes-guardadas").setViewName("mi-perfil/imagenes-guardadas");
+        registry.addViewController("busqueda").setViewName("busqueda");
+        registry.addViewController("ajustes").setViewName("ajustes");
+        registry.addViewController("asistencia").setViewName("asistencia");
+
 
     }
 }
