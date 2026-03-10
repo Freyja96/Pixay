f35ef165ce96e75e53e6d78937c40d814efaf4a5
Naomii-Manss <naomiimansaray@gmail.com>
2026-03-10 13:21:09 +0100

    Pimer intento a empezar a edita en la rama lado-naomi

--- PixayAPI/src/main/java/es/daw/foodexpressapi/PixayApiApplication.java
+++ PixayAPI/src/main/java/es/daw/foodexpressapi/PixayApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FoodExpressApiApplication {
+public class PixayApiApplication{
 
     public static void main(String[] args) {
-        SpringApplication.run(FoodExpressApiApplication.class, args);
+        SpringApplication.run(PixayApiApplication.class, args);
     }
 
 }
