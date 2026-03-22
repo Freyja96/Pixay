4cd56defa04d3bad387bf82190b4f8075e70a1c9
Freyja96 <cristina.oue@gmail.com>
2026-03-22 16:41:30 +0100

    Mejora de la BDD: Incluidas tablas categories y subcategories e inserción de datos

--- PixayAPI/src/main/resources/data.sql
+++ PixayAPI/src/main/resources/data.sql
@@ -6,3 +6,22 @@ MERGE INTO users (id, username, password, email, role_id) KEY(id) VALUES
                                                                (2, 'juan', '1234', 'juan@correo.com', 2),
                                                                (3, 'maria', '1234', 'maria@correo.com', 2),
                                                                (4, 'julia', '1234', 'julia@correo.com', 2);
+
+-- Categorías Base
+MERGE INTO categories (id, name) KEY(id) VALUES (1, 'Ilustración'),
+                                                (2, 'Fotografía');
+
+MERGE INTO subcategories (id, name, category) KEY(id) VALUES (1, 'Retrato', 'Fotografía'),
+                                                             (2, 'Paisaje', 'Fotografía'),
+                                                             (3, 'Fotografía urbana', 'Fotografía'),
+                                                             (4, 'Macro', 'Fotografía'),
+                                                             (5, 'Arquitectura', 'Fotografía'),
+                                                             (6, 'Moda', 'Fotografía');
+
+-- Subcategorías para 'Ilustración'
+MERGE INTO subcategories (id, name, category) KEY(id) VALUES (7, 'Arte conceptual', 'Ilustración'),
+                                                             (8, 'Pixel Art', 'Ilustración'),
+                                                             (9, 'Fan Art', 'Ilustración'),
+                                                             (10, 'Acuarela Digital', 'Ilustración'),
+                                                             (11, 'Modelado 3D', 'Ilustración'),
+                                                             (12, 'Digital Art', 'Ilustración');
--- PixayAPI/src/main/resources/schema.sql
+++ PixayAPI/src/main/resources/schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS roles (
                                      id BIGINT AUTO_INCREMENT PRIMARY KEY,
                                      name VARCHAR(50) NOT NULL UNIQUE
-    );
+);
 
 CREATE TABLE IF NOT EXISTS users (
                                     id BIGINT AUTO_INCREMENT PRIMARY KEY,
@@ -10,8 +10,18 @@ CREATE TABLE IF NOT EXISTS users (
                                     email VARCHAR(100) NOT NULL,
                                     role_id BIGINT NOT NULL,
                                     FOREIGN KEY (role_id) REFERENCES roles(id)
-    );
+);
 
+CREATE TABLE IF NOT EXISTS categories (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL UNIQUE
+);
+CREATE TABLE IF NOT EXISTS subcategories (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL UNIQUE,
+    category VARCHAR(50) NOT NULL,
+    FOREIGN KEY (category) REFERENCES categories(name)
+);
 --tabla de imágenes!!
 -- @Id
 -- @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -27,6 +37,8 @@ CREATE TABLE IF NOT EXISTS users_images (
     title VARCHAR(100),
     content LONGBLOB NOT NULL,
     user_id BIGINT NOT NULL,
-    category VARCHAR(50),
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    category VARCHAR(50) NOT NULL,
+    subcategory VARCHAR(50),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (category) REFERENCES categories(name)
 )
